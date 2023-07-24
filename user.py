5c9d8dcd4b23e1803c9523e675d7c3daf9265a07
Marvid007 <117814556+Marvid007@users.noreply.github.com>
2023-07-24 03:31:19 +0100

    Update user.py

--- user.py
+++ user.py
@@ -9,21 +9,21 @@ from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
-        """Representation of a user """
-            if models.storage_t == 'db':
-                        __tablename__ = 'users'
-                                email = Column(String(128), nullable=False)
-                                        password = Column(String(128), nullable=False)
-                                                first_name = Column(String(128), nullable=True)
-                                                        last_name = Column(String(128), nullable=True)
-                                                                places = relationship("Place", backref="user")
-                                                                        reviews = relationship("Review", backref="user")
-                                                                            else:
-                                                                                        email = ""
-                                                                                                password = ""
-                                                                                                        first_name = ""
-                                                                                                                last_name = ""
+    """Representation of a user """
+    if models.storage_t == 'db':
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship("Place", backref="user")
+        reviews = relationship("Review", backref="user")
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
 
-                                                                                                                    def __init__(self, *args, **kwargs):
-                                                                                                                                """initializes user"""
-                                                                                                                                        super().__init__(*args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        """initializes user"""
+        super().__init__(*args, **kwargs)
