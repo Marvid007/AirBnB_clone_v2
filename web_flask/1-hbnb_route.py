2362f157c41a72685f180298718522bb0a597487
Marvid007 <117814556+Marvid007@users.noreply.github.com>
2023-07-24 02:15:06 +0100

    Update 1-hbnb_route.py

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -3,25 +3,25 @@
 
 The application listens on 0.0.0.0, port 5000.
 Routes:
-            /: Displays 'Hello HBNB!'.
-                        /hbnb: Displays 'HBNB'.
-                                    """
-                                                from flask import Flask
+        /: Displays 'Hello HBNB!'.
+            /hbnb: Displays 'HBNB'.
+            """
+            from flask import Flask
 
-                                                            app = Flask(__name__)
-
-
-                                                                        @app.route("/", strict_slashes=False)
-                                                                                    def hello_hbnb():
-                                                                                                        """Displays 'Hello HBNB!'."""
-                                                                                                                        return "Hello HBNB!"
+            app = Flask(__name__)
 
 
-                                                                                                                                @app.route("/hbnb", strict_slashes=False)
-                                                                                                                                            def hbnb():
-                                                                                                                                                                """Displays 'HBNB'."""
-                                                                                                                                                                                return "HBNB"
+            @app.route("/", strict_slashes=False)
+            def hello_hbnb():
+                """Displays 'Hello HBNB!'."""
+                return "Hello HBNB!"
 
 
-                                                                                                                                                                                        if __name__ == "__main__":
-                                                                                                                                                                                                            app.run(host="0.0.0.0")
+            @app.route("/hbnb", strict_slashes=False)
+            def hbnb():
+                """Displays 'HBNB'."""
+                return "HBNB"
+
+
+            if __name__ == "__main__":
+                app.run(host="0.0.0.0")
