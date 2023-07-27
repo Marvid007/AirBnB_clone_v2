924ceb847e4cba52f8470ff298d7fed3ccdfb125
Marvid007 <117814556+Marvid007@users.noreply.github.com>
2023-07-27 01:35:22 +0100

    Update 2-c_route.py

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -3,33 +3,33 @@
 
 The application listens on 0.0.0.0, port 5000.
 Routes:
-        /: Displays 'Hello HBNB!'.
-            /hbnb: Displays 'HBNB'.
-                /c/<text>: Displays 'C' followed by the value of <text>.
-                """
-                from flask import Flask
+    /: Displays 'Hello HBNB!'.
+    /hbnb: Displays 'HBNB'.
+    /c/<text>: Displays 'C' followed by the value of <text>.
+"""
+from flask import Flask
 
-                app = Flask(__name__)
-
-
-                @app.route("/", strict_slashes=False)
-                def hello_hbnb():
-                    """Displays 'Hello HBNB!'."""
-                    return "Hello HBNB!"
+app = Flask(__name__)
 
 
-                @app.route("/hbnb", strict_slashes=False)
-                def hbnb():
-                    """Displays 'HBNB'."""
-                    return "HBNB"
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Displays 'Hello HBNB!'."""
+    return "Hello HBNB!"
 
 
-                @app.route("/c/<text>", strict_slashes=False)
-                def c(text):
-                    """Displays 'C' followed by the value of <text>."""
-                    text = text.replace("_", " ")
-                    return "C {}".format(text)
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Displays 'HBNB'."""
+    return "HBNB"
 
 
-                if __name__ == "__main__":
-                    app.run(host="0.0.0.0")
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """Displays 'C' followed by the value of <text>."""
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
