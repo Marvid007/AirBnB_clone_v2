7cbcbc3cd3af443c20d3bffcf89f27a1a4f5fb52
Marvid007 <117814556+Marvid007@users.noreply.github.com>
2023-07-27 01:44:38 +0100

    Update 6-number_odd_or_even.py

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -3,15 +3,15 @@
 
 The application listens on 0.0.0.0, port 5000.
 Routes:
-        /: Displays 'Hello HBNB!'.
-        /hbnb: Displays 'HBNB'.
-        /c/<text>: Displays 'C' followed by the value of <text>.
-        /python/(<text>): Displays 'Python' followed by the value of <text>.
-        /number/<n>: Displays 'n is a number' only if <n> is an integer.
-        /number_template/<n>: Displays an HTML page only if <n> is an integer.
-            - Displays the value of <n> in the body.
-        /number_odd_or_even/<n>: Displays an HTML page only if <n> is an integer.
-            - States whether <n> is even or odd in the body.
+    /: Displays 'Hello HBNB!'.
+    /hbnb: Displays 'HBNB'.
+    /c/<text>: Displays 'C' followed by the value of <text>.
+    /python/(<text>): Displays 'Python' followed by the value of <text>.
+    /number/<n>: Displays 'n is a number' only if <n> is an integer.
+    /number_template/<n>: Displays an HTML page only if <n> is an integer.
+        - Displays the value of <n> in the body.
+    /number_odd_or_even/<n>: Displays an HTML page only if <n> is an integer.
+        - States whether <n> is even or odd in the body.
 """
 from flask import Flask
 from flask import render_template
@@ -25,6 +25,7 @@ app.jinja_env.lstrip_blocks = True
 def hello_hbnb():
     """Displays 'Hello HBNB!'"""
     return "Hello HBNB!"
+
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
